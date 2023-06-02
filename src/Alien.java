b4b62ef9539e3d8580a29a88c2513f88d3453b0a
Daddy <Daddy@vivs-macpro-13>
2023-06-01 17:23:20 -0700

    did this 6/1

---
+++ src/Alien.java
@@ -0,0 +1,19 @@
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class Alien extends GameObject{
+
+	public Alien(int x, int y, int width, int height, int speed, boolean isActive) {
+		super(x, y, width, height, speed, isActive);
+		// TODO Auto-generated constructor stub
+		speed = 1;
+	}
+	void update() {
+		
+	}
+	void draw(Graphics g) {
+		g.setColor(Color.YELLOW);
+        g.fillRect(x, y, width, height);
+	}
+
+}
